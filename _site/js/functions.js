553d6b87f3964e557fc82a9d4c5615d1ae1b28f6
Tim <timbog80@gmail.com>
2014-09-27 21:00:15 -0700

    major update and polishing

--- _site/js/functions.js
+++
@@ -1,8 +0,0 @@
-$(function() {
-  $(window).scroll(function() {
-    if($(this).scrollTop() > 10)
-      $('.site-header, .page-content').addClass('smaller');
-    else 
-      $('.site-header, .page-content').removeClass('smaller');
-  });
-});
